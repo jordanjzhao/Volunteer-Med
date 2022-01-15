c0a6e1ba6ce46ec66c2145a12d9bfe58c615f66f
Jordan Zhao <jordanzhao@Jordans-MacBook-Pro.local>
2022-01-15 14:22:48 -0800

    Created loading screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 //import './App.css';
+import React from "react";
+import Loading from "./components/Loading";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <link href="../dist/output.css" rel="stylesheet"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
-
-      </body>
+      <Loading />
     </div>
   );
 }
---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logoTest from '../images/logotest.jpg';
+
+export default function Loading() {
+    return (
+        <div>
+            <img className="testlogo" src={logoTest}/>
+        </div>
+    )
+}
