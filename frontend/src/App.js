05e4473df6163a945480788fa9cc10545241792d
phillip.tellier <backphillip@gmail.com>
2022-01-15 13:25:13 -0800

    added tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link href="../dist/output.css" rel="stylesheet"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
