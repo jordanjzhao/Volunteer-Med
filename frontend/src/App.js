95c548c64e9d1f8d215b36b5a4aa65725c2595c7
Jordan Zhao <jordanzhao@Jordans-MacBook-Pro.local>
2022-01-15 18:17:34 -0800

    Created SignUp Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@
 import React from "react";
 import Loading from "./components/Loading";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <Loading />
+      <Login />
+      <SignUp />
     </div>
   );
 }
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,22 +1,18 @@
 import React from "react";
 import vectorDesign from '../images/Vector_1.png';
 
-export default function Login() {
+export default function SignUp() {
     return (
         <div>
         <header className="header">
             <img className="login--vector" src={vectorDesign}/>
-            <div class="centered-left">Login</div>
+            <div class="centered-left">Sign Up</div>
         </header>
         <main>
             <form className="form">
                 <div>
-                    <h1>Phone Number</h1>
-                    <input className="form--input" placeholder="Phone number" type="text" />
-                </div>
-                <div> 
-                    <h1>Verification Number</h1>
-                    <input className="form--input" placeholder="Verification" type="text" />
+                    <h1>Are you a...</h1>
+                    <input className="form--input" placeholder="Select position" type="text" />
                 </div>
                 <div>
                     <button className="form--button">></button>
