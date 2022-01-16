663c44e2e0ff9df5eec2781a25e628369c20ddcd
phillip.tellier <backphillip@gmail.com>
2022-01-15 16:35:53 -0800

    frontend2

---
+++ frontend2/my-app/src/LoginScreen.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const loginscreen = () => {
+  return (
+    <div className='flex bg-blue-700'>
+      <div className='content-center'>
+        <img alt='HeyDoc! Logo'></img>
+        <button className='py-0.5'>Signup</button>
+        <button className='py-0.5'>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default loginscreen
